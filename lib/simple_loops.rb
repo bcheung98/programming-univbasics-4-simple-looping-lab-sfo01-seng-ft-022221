5cb43d77bfd6d1703bb72edb56c1fffc7e45a2e2
Brian Cheung <briantcheung@gmail.com>
2021-01-19 19:57:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,5 +3,13 @@ def loop_message_five_times(string)
   count = 5
   while count > 0 do
     puts string
+    count -=1
+  end
+end
+
+def loop_message_n_times(string, n)
+  while n > 0 do
+    puts string
+    n -=1
   end
 end
